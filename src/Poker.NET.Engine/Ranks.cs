98401ac6d6dab943ab554b21d6713b13ef26ce51
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-12-30 23:06:09 +0100

    Added ranks and suits enums.

---
+++ src/Poker.NET.Engine/Ranks.cs
@@ -0,0 +1,19 @@
+namespace Poker.NET.Engine;
+
+public enum Ranks : ushort
+{
+    None,
+    Twos,
+    Threes,
+    Fours,
+    Fives,
+    Sixes,
+    Sevens,
+    Eights,
+    Nines,
+    Tens,
+    Jacks,
+    Queens,
+    Kings,
+    Aces
+}
