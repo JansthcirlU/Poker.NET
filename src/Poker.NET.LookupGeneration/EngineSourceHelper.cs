16b795bb13f7ec6e9ed939046ea25a5a2e6d2f76
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-01-06 21:27:00 +0100

    Added methods to generate scoring method source with simple tests.

--- src/Poker.NET.LookupGeneration/EngineSourceHelper.cs
+++ src/Poker.NET.LookupGeneration/EngineSourceHelper.cs
@@ -1,12 +1,12 @@
 ﻿using System.Text;
 using Poker.NET.Engine;
+using Poker.NET.Engine.Helpers;
 
 namespace Poker.NET.LookupGeneration;
 
 public static class EngineSourceHelper
 {
-    private static Dictionary<string, string> RankNamePlurals
-        => new()
+    private static readonly Dictionary<string, string> RankNamePlurals = new()
         {
             { "Two", "Twos" },
             { "Three", "Threes" },
@@ -22,6 +22,14 @@ public static class EngineSourceHelper
             { "King", "Kings" },
             { "Ace", "Aces" },
         };
+    
+    public static Dictionary<Cards, int> ToSuitAndRankDictionary(this Cards cards)
+        => HandsGenerator.GetAllSuits()
+            .Concat(HandsGenerator.GetAllRanks())
+            .ToDictionary(
+                suitOrRank => suitOrRank,
+                suitOrRank => (cards & suitOrRank).GetCardCount()
+            );
 
     public static string GenerateCardsEnum()
     {
@@ -44,6 +52,209 @@ public static class EngineSourceHelper
                 .AddCategory("Straight Flush", HandsGenerator.GetAllStraightFlushes())
             .AppendLine("}");
         return cardsEnumBuilder.ToString();
+    }
+
+    public static string GenerateGetTwoPairRanks()
+    {
+        List<Cards> allRanks = HandsGenerator.GetAllRanks().ToList();
+        Dictionary<(Rank, Rank), IEnumerable<Cards>> allTwoPairs = HandsGenerator.GetAllTwoPairs()
+            .Select(twoPair => new
+            {
+                Cards = twoPair,
+                Ranks = twoPair
+                    .ToSuitAndRankDictionary()
+                    .Where(kvp => kvp.Value == 2 && allRanks.Contains(kvp.Key))
+                    .Select(kvp => kvp.Key switch
+                    {
+                        Cards.Twos => Rank.Two,
+                        Cards.Threes => Rank.Three,
+                        Cards.Fours => Rank.Four,
+                        Cards.Fives => Rank.Five,
+                        Cards.Sixes => Rank.Six,
+                        Cards.Sevens => Rank.Seven,
+                        Cards.Eights => Rank.Eight,
+                        Cards.Nines => Rank.Nine,
+                        Cards.Tens => Rank.Ten,
+                        Cards.Jacks => Rank.Jack,
+                        Cards.Queens => Rank.Queen,
+                        Cards.Kings => Rank.King,
+                        Cards.Aces => Rank.AceHigh,
+                        _ => Rank.None
+                    })
+                    .OrderByDescending(k => k)
+                    .ToList()
+            })
+            .GroupBy(twoPair => (twoPair.Ranks[0], twoPair.Ranks[1]))
+            .ToDictionary(group => group.Key, group => group.Select(g => g.Cards));
+
+        StringBuilder methodBuilder = new();
+        methodBuilder
+            .AppendLine("public static (Rank HighestPairRank, Rank LowestPairRank) GetTwoPairRanks(Cards twoPair)")
+            .AppendLine("    => twoPair switch")
+            .AppendLine("    {");
+        foreach (((Rank lowPairRank, Rank highPairRank), IEnumerable<Cards> twoPairs) in allTwoPairs)
+        {
+            string ranks = $"(Rank.{lowPairRank}, Rank.{highPairRank})";
+            string cards = string.Join(" or ", twoPairs.Select(twoPair => $"Cards.{twoPair}"));
+            methodBuilder.AppendLine($"        {cards} => {ranks},");
+        }
+        methodBuilder
+            .AppendLine("        _ => throw new ArgumentException($\"{twoPair.ToCardString()} does not contain exactly two pairs.\", nameof(twoPair))")
+            .AppendLine("    };");
+        return methodBuilder.ToString();
+    }
+
+    public static string GenerateStraightRanks()
+    {
+        Dictionary<Rank, List<Cards>> straightsByRank = [];
+        foreach (Cards straight in HandsGenerator.GetAllStraights())
+        {
+            List<Rank> ranks = straight
+                .GetIndividualCards()
+                .Select(c => c.GetRank())
+                .OrderByDescending(r => r)
+                .ToList();
+            Rank highest = ranks.Contains(Rank.AceHigh) && ranks.Contains(Rank.Five)
+                ? Rank.Five
+                : ranks.First();
+            
+            if (!straightsByRank.ContainsKey(highest)) straightsByRank[highest] = [];
+            straightsByRank[highest].Add(straight);
+        }
+
+        StringBuilder methodBuilder = new();
+        methodBuilder
+            .AppendLine("public static Rank GetStraightRank(Cards straight)")
+            .AppendLine("    => straight switch")
+            .AppendLine("    {");
+        foreach ((Rank highestRank, IEnumerable<Cards> straights) in straightsByRank)
+        {
+            string rank = $"Rank.{highestRank}";
+            string cards = string.Join(" or ", straights.Select(straight => $"Cards.{straight}"));
+            methodBuilder.AppendLine($"        {cards} => {rank},");
+        }
+        methodBuilder
+            .AppendLine("        _ => throw new ArgumentException($\"{straight.ToCardString()} is not a straight.\", nameof(straight))")
+            .AppendLine("    };");
+        return methodBuilder.ToString();
+    }
+
+    public static string GenerateStraightFlushRanks()
+    {
+        Dictionary<Rank, List<Cards>> straightsByRank = [];
+        foreach (Cards straight in HandsGenerator.GetAllStraightFlushes())
+        {
+            List<Rank> ranks = straight
+                .GetIndividualCards()
+                .Select(c => c.GetRank())
+                .OrderByDescending(r => r)
+                .ToList();
+            Rank highest = ranks.Contains(Rank.AceHigh) && ranks.Contains(Rank.Five)
+                ? Rank.Five
+                : ranks.First();
+            
+            if (!straightsByRank.ContainsKey(highest)) straightsByRank[highest] = [];
+            straightsByRank[highest].Add(straight);
+        }
+
+        StringBuilder methodBuilder = new();
+        methodBuilder
+            .AppendLine("public static Rank GetStraightFlushRank(Cards straightFlush)")
+            .AppendLine("    => straightFlush switch")
+            .AppendLine("    {");
+        foreach ((Rank highestRank, IEnumerable<Cards> straights) in straightsByRank)
+        {
+            string rank = $"Rank.{highestRank}";
+            string cards = string.Join(" or ", straights.Select(straight => $"Cards.{straight}"));
+            methodBuilder.AppendLine($"        {cards} => {rank},");
+        }
+        methodBuilder
+            .AppendLine("        _ => throw new ArgumentException($\"{straightFlush.ToCardString()} is not a straight flush.\", nameof(straightFlush))")
+            .AppendLine("    };");
+        return methodBuilder.ToString();
+    }
+
+    public static string GenerateFlushRanks()
+    {
+        Dictionary<(Rank, Rank, Rank, Rank, Rank), IEnumerable<Cards>> allFlushes = HandsGenerator.GetAllFlushes()
+            .Select(flush => new
+            {
+                Cards = flush,
+                Ranks = flush
+                    .GetIndividualCards()
+                    .Select(c => c.GetRank())
+                    .OrderByDescending(r => r)
+                    .ToList()
+            })
+            .GroupBy(flush => (flush.Ranks[0], flush.Ranks[1], flush.Ranks[2], flush.Ranks[3], flush.Ranks[4]))
+            .ToDictionary(group => group.Key, group => group.Select(g => g.Cards));
+
+        StringBuilder methodBuilder = new();
+        methodBuilder
+            .AppendLine("public static (Rank Highest, Rank Second, Rank Third, Rank Fourth, Rank Fifth) GetFlushRanks(Cards flush)")
+            .AppendLine("    => flush switch")
+            .AppendLine("    {");
+        foreach (((Rank highest, Rank second, Rank third, Rank fourth, Rank fifth), IEnumerable<Cards> flushes) in allFlushes)
+        {
+            string ranks = $"(Rank.{highest}, Rank.{second}, Rank.{third}, Rank.{fourth}, Rank.{fifth})";
+            string cards = string.Join(" or ", flushes.Select(flush => $"Cards.{flush}"));
+            methodBuilder.AppendLine($"        {cards} => {ranks},");
+        }
+        methodBuilder
+            .AppendLine("        _ => throw new ArgumentException($\"{flush.ToCardString()} is not a flush.\", nameof(flush))")
+            .AppendLine("    };");
+        return methodBuilder.ToString();
+    }
+
+    public static string GenerateFullHouseRanks()
+    {
+        List<Cards> allRanks = HandsGenerator.GetAllRanks().ToList();
+        Dictionary<(Rank, Rank), IEnumerable<Cards>> allFullHouses = HandsGenerator.GetAllFullHouse()
+            .Select(fullHouse => new
+            {
+                Cards = fullHouse,
+                Ranks = fullHouse
+                    .ToSuitAndRankDictionary()
+                    .Where(kvp => allRanks.Contains(kvp.Key))
+                    .ToDictionary(
+                        kvp => kvp.Key switch
+                        {
+                            Cards.Twos => Rank.Two,
+                            Cards.Threes => Rank.Three,
+                            Cards.Fours => Rank.Four,
+                            Cards.Fives => Rank.Five,
+                            Cards.Sixes => Rank.Six,
+                            Cards.Sevens => Rank.Seven,
+                            Cards.Eights => Rank.Eight,
+                            Cards.Nines => Rank.Nine,
+                            Cards.Tens => Rank.Ten,
+                            Cards.Jacks => Rank.Jack,
+                            Cards.Queens => Rank.Queen,
+                            Cards.Kings => Rank.King,
+                            Cards.Aces => Rank.AceHigh,
+                            _ => Rank.None
+                        },
+                        kvp => kvp.Value
+                    )
+            })
+            .GroupBy(x => (x.Ranks.Single(kvp => kvp.Value == 3).Key, x.Ranks.Single(kvp => kvp.Value == 2).Key))
+            .ToDictionary(group => group.Key, group => group.Select(g => g.Cards));
+        
+        StringBuilder methodBuilder = new();
+        methodBuilder
+            .AppendLine("public static (Rank ThreeOfAKindRank, Rank PairRank) GetFullHouseRanks(Cards fullHouse)")
+            .AppendLine("    => fullHouse switch")
+            .AppendLine("    {");
+        foreach (((Rank threeOfAKindRank, Rank pairRank), IEnumerable<Cards> fullHouses) in allFullHouses)
+        {
+            string ranks = $"(Rank.{threeOfAKindRank}, Rank.{pairRank})";
+            string cards = string.Join(" or ", fullHouses.Select(fullHouse => $"Cards.{fullHouse}"));
+            methodBuilder.AppendLine($"        {cards} => {ranks},");
+        }
+        methodBuilder
+            .AppendLine("        _ => throw new ArgumentException($\"{fullHouse.ToCardString()} is not a full house.\", nameof(fullHouse))")
+            .AppendLine("    };");
+        return methodBuilder.ToString();
     }
 
     private static StringBuilder AddSingleCards(this StringBuilder cardsEnumBuilder)
--- tests/Poker.NET.LookupGeneration.Tests/EngineSourceHelperTests.cs
+++ tests/Poker.NET.LookupGeneration.Tests/EngineSourceHelperTests.cs
@@ -11,4 +11,54 @@ public class EngineSourceHelperTests
         // Assert
         Assert.NotEmpty(cardsEnum);
     }
+
+    [Fact]
+    public void GenerateGetTwoPairRanks_ShouldContainAllTwoPairRanks()
+    {
+        // Arrange
+        string getTwoPairRanks = EngineSourceHelper.GenerateGetTwoPairRanks();
+
+        // Assert
+        Assert.NotEmpty(getTwoPairRanks);
+    }
+
+    [Fact]
+    public void GenerateStraightRanks_ShouldContainAllStraightRanks()
+    {
+        // Arrange
+        string straightRanks = EngineSourceHelper.GenerateStraightRanks();
+
+        // Assert
+        Assert.NotEmpty(straightRanks);
+    }
+
+    [Fact]
+    public void GenerateStraightFlushRanks_ShouldContainAllStraightFlushRanks()
+    {
+        // Arrange
+        string straightFlushRanks = EngineSourceHelper.GenerateStraightFlushRanks();
+
+        // Assert
+        Assert.NotEmpty(straightFlushRanks);
+    }
+
+    [Fact]
+    public void GenerateFlushRanks_ShouldContainAllFlushRanks()
+    {
+        // Arrange
+        string flushRanks = EngineSourceHelper.GenerateFlushRanks();
+
+        // Assert
+        Assert.NotEmpty(flushRanks);
+    }
+
+    [Fact]
+    public void GenerateFullHouseRanks_ShouldContainAllFullHouseRanks()
+    {
+        // Arrange
+        string fullHouseRanks = EngineSourceHelper.GenerateFullHouseRanks();
+
+        // Assert
+        Assert.NotEmpty(fullHouseRanks);
+    }
 }
