01f74c749d798d9e147daa85e64fd76b5fbad9f8
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-01-09 00:21:51 +0100

    Added tests for HoldemHand.

--- src/Poker.NET.Engine/HoldemHand.cs
+++ src/Poker.NET.Engine/HoldemHand.cs
@@ -13,10 +13,14 @@ public readonly record struct HoldemHand
     }
     public HoldemHand(Cards holeCards, Cards communityCards)
     {
+        if ((holeCards & communityCards) != Cards.None) throw new ArgumentException($"Hole cards and community cards must not overlap in {holeCards.ToCardString()} and {communityCards.ToCardString()}", nameof(communityCards));
         if (holeCards.GetCardCount() != 2) throw new ArgumentException($"There are not exactly two hole cards in {holeCards.ToCardString()}", nameof(holeCards));
         if (communityCards.GetCardCount() != 5) throw new ArgumentException($"There are not exactly five community cards in {communityCards.ToCardString()}", nameof(communityCards));
 
         HoleCards = holeCards;
         CommunityCards = communityCards;
     }
+
+    public override string ToString()
+        => (HoleCards | CommunityCards).ToCardString();
 }
--- tests/Poker.NET.Engine.Tests/HoldemHandTests.cs
+++ tests/Poker.NET.Engine.Tests/HoldemHandTests.cs
@@ -1,9 +1,60 @@
 using Poker.NET.Engine.Helpers;
+using Poker.NET.LookupGeneration;
 
 namespace Poker.NET.Engine.Tests;
 
 public class HoldemHandTests
 {
+    public const Cards TwoCardHand = Cards.TwoOfClubs | Cards.TwoOfSpades;
+    public const Cards FiveCardHand = Cards.TwoOfClubs | Cards.TwoOfSpades | Cards.FourOfHearts | Cards.FiveOfDiamonds | Cards.SevenOfClubs;
+
+    private static readonly List<Cards> AllOneCardHands = HandsGenerator.GetAllKCardHands(1).Take(10).ToList();
+    private static readonly List<Cards> AllTwoCardHands = HandsGenerator.GetAllKCardHands(2).Take(10).ToList();
+    private static readonly List<Cards> AllThreeCardHands = HandsGenerator.GetAllKCardHands(3).Take(10).ToList();
+    private static readonly List<Cards> AllFourCardHands = HandsGenerator.GetAllKCardHands(4).Take(10).ToList();
+    private static readonly List<Cards> AllFiveCardHands = HandsGenerator.GetAllKCardHands(5).Take(10).ToList();
+    private static readonly List<Cards> AllSixCardHands = HandsGenerator.GetAllKCardHands(6).Take(10).ToList();
+    private static readonly List<Cards> AllSevenCardHands = HandsGenerator.GetAllKCardHands(7).ToList();
+    private static readonly List<Cards> AllButTwoCardHands = AllOneCardHands
+        .Concat(AllThreeCardHands)
+        .Concat(AllFourCardHands)
+        .Concat(AllFiveCardHands)
+        .Concat(AllSixCardHands)
+        .Concat(AllSevenCardHands.Take(10))
+        .ToList();
+    private static readonly List<Cards> AllButFiveCardHands = AllOneCardHands
+        .Concat(AllTwoCardHands)
+        .Concat(AllThreeCardHands)
+        .Concat(AllFourCardHands)
+        .Concat(AllSixCardHands)
+        .Concat(AllSevenCardHands.Take(10))
+        .ToList();
+    private static readonly List<(Cards HoleCards, Cards CommunityCards)> CombinedTwoAndFiveCardHands = AllTwoCardHands
+        .SelectMany(holeCards => AllFiveCardHands.Select(communityCards => (holeCards, communityCards)))
+        .ToList();
+
+    public static IEnumerable<object[]> AllButTwoCardHandsData
+        => AllButTwoCardHands
+            .Where(c => (c & FiveCardHand) == Cards.None)
+            .Select(h => new object[] { h, FiveCardHand });
+    public static IEnumerable<object[]> AllButFiveCardHandsData
+        => AllButFiveCardHands
+            .Where(c => (c & TwoCardHand) == Cards.None)
+            .Select(h => new object[] { TwoCardHand, h });
+    public static IEnumerable<object[]> GetInvalidHoleCardAndCommunityCards
+        => CombinedTwoAndFiveCardHands
+            .Where(hand => !AllSevenCardHands.Contains(hand.HoleCards | hand.CommunityCards))
+            .Select(hand => new object[] { hand.HoleCards, hand.CommunityCards });
+    public static IEnumerable<object[]> GetValidHoleCardAndCommunityCards
+        => AllSevenCardHands
+            .Take(10)
+            .Select(cards => new
+            {
+                HoleCards = HandsGenerator.GetAllKCardHands(2).First(mask => (cards & mask) == mask),
+                Cards = cards
+            })
+            .Select(x => new object[] { x.HoleCards, x.Cards & ~x.HoleCards });
+
     [Fact]
     public void HoldemHand_WhenConstructedWithDefaultConstructor_ShouldThrow()
     {
@@ -17,14 +68,9 @@ public class HoldemHandTests
         Assert.StartsWith("You must not use the default constructor to create a HoldemHand.", ex.Message);
     }
 
-    // TODO: Import Poker.NET.LookupGeneration and use HandsGenerator theory data for the following tests
-
-    // Write tests for all single-card, three-card, ..., seven-card combinations (not two-card)
-    // to check if hole card assignment throws exception
     [Theory]
-    public void HoldemHand_WhenConstructedWithInvalidHoleCards_ShouldThrow(
-        Cards holeCards,
-        Cards communityCards)
+    [MemberData(nameof(AllButTwoCardHandsData))]
+    public void HoldemHand_WhenConstructedWithInvalidHoleCards_ShouldThrow(Cards holeCards, Cards communityCards)
     {
         // Act
         ArgumentException? ex = Assert.Throws<ArgumentException>(
@@ -36,12 +82,9 @@ public class HoldemHandTests
         Assert.StartsWith($"There are not exactly two hole cards in {holeCards.ToCardString()}", ex.Message);
     }
 
-    // Write tests for all single-card, two-card, ..., seven-card combinations (not five-card)
-    // to check if community card assignment throws exception
     [Theory]
-    public void HoldemHand_WhenConstructedWithInvalidCommunityCards_ShouldThrow(
-        Cards holeCards,
-        Cards communityCards)
+    [MemberData(nameof(AllButFiveCardHandsData))]
+    public void HoldemHand_WhenConstructedWithInvalidCommunityCards_ShouldThrow(Cards holeCards, Cards communityCards)
     {
         // Act
         ArgumentException? ex = Assert.Throws<ArgumentException>(
@@ -53,18 +96,29 @@ public class HoldemHandTests
         Assert.StartsWith($"There are not exactly five community cards in {communityCards.ToCardString()}", ex.Message);
     }
 
-    // Write tests for all two-card and five-card combinations
-    // to check if HoldemHand construction succeeds
     [Theory]
-    public void HoldemHand_WhenConstructedWithValidCards_ShouldSucceed(
-        Cards holeCards,
-        Cards communityCards)
+    [MemberData(nameof(GetInvalidHoleCardAndCommunityCards))]
+    public void HoldemHand_WhenConstructedWithOverlappingHoleAndCommunityCards_ShouldThrow(Cards holeCards, Cards communityCards)
     {
         // Act
-        HoldemHand hand = new(holeCards, communityCards);
+        ArgumentException? ex = Assert.Throws<ArgumentException>(
+            () => new HoldemHand(holeCards, communityCards)
+        );
 
         // Assert
-        Assert.Equal(holeCards, hand.HoleCards);
-        Assert.Equal(communityCards, hand.CommunityCards);
+        Assert.NotNull(ex);
+        Assert.StartsWith($"Hole cards and community cards must not overlap in {holeCards.ToCardString()} and {communityCards.ToCardString()}", ex.Message);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetValidHoleCardAndCommunityCards))]
+    public void HoldemHand_WhenConstructedWithValidCards_ShouldSucceed(Cards holeCards, Cards communityCards)
+    {
+        // Act
+        HoldemHand holdemHand = new(holeCards, communityCards);
+
+        // Assert
+        Assert.Equal(holeCards, holdemHand.HoleCards);
+        Assert.Equal(communityCards, holdemHand.CommunityCards);
     }
 }
