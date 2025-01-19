f1b05ac06922abed17f2986a841e14914461a505
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-01-19 13:59:35 +0100

    Implemented worker logic.

--- src/Poker.NET.Worker/Worker.cs
+++ src/Poker.NET.Worker/Worker.cs
@@ -1,23 +1,89 @@
+using Poker.NET.Engine;
+using Poker.NET.LookupGeneration;
+using Poker.NET.LookupGeneration.Data;
+using Poker.NET.Worker.Helpers;
+
 namespace Poker.NET.Worker;
 
 public class Worker : BackgroundService
 {
+    private readonly IHandComparisonFileHelper _fileHelper;
+    private readonly IHostApplicationLifetime _lifetime;
+    private readonly IConfiguration _configuration;
     private readonly ILogger<Worker> _logger;
+    private HandComparisonTree? _lastTree;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(
+        IHandComparisonFileHelper fileHelper,
+        IHostApplicationLifetime lifetime,
+        IConfiguration configuration,
+        ILogger<Worker> logger)
     {
+        _fileHelper = fileHelper;
+        _lifetime = lifetime;
+        _configuration = configuration;
         _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        // If more than three checkpoint directories exist, delete the oldest one
+        string checkpointsPath = _configuration["CheckpointsPath"]!;
+        string[] directories = Directory.Exists(checkpointsPath)
+            ? [.. Directory.GetDirectories(checkpointsPath).OrderByDescending(d => d)]
+            : [];
+        if (directories.Length > 2)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
+            string oldestDirectory = directories.Last();
+            Directory.Delete(oldestDirectory, true);
+            _logger.LogInformation("Deleted oldest checkpoint directory {OldestDirectory}.", oldestDirectory);
+        }
+
+        // Load latest checkpoint directory if it exists
+        string? latestDirectory = directories.FirstOrDefault();
+        if (latestDirectory is not null)
+        {
+            _logger.LogInformation("Loading latest checkpoint directory {LatestDirectory}.", latestDirectory);
+            _lastTree = await _fileHelper.LoadFromFileAsync(latestDirectory, stoppingToken);
+        }
+
+        // Create checkpoint directory for current run
+        string currentDateTime = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+        string directoryPath = Path.Combine(checkpointsPath, currentDateTime);
+        Directory.CreateDirectory(directoryPath);
+        _logger.LogInformation("Created checkpoint directory {DirectoryPath}.", directoryPath);
+
+        // Generate hand comparison tree
+        try
+        {
+            int skippable = (int)_lastTree!.Count;
+            IEnumerable<HoldemHand> allHands = HandsGenerator.GetAllHands().Skip(skippable);
+            _logger.LogInformation("Starting to generate hand comparison tree for all hands.");
+            int chunksProcessed = 0;
+            int chunkSize = 500;
+            foreach (IEnumerable<HoldemHand> chunk in allHands.Chunk(chunkSize))
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Processing hands {ChunkStart} through {ChunkEnd}.", skippable + chunksProcessed * chunkSize, skippable + (chunksProcessed + 1) * chunkSize);
+                foreach (HoldemHand hand in chunk)
+                {
+                    _lastTree.Add(hand);
+                    if (stoppingToken.IsCancellationRequested) break;
+                }
+                _logger.LogInformation("Writing chunk {ChunkNumber} to file.", chunksProcessed);
+                await _fileHelper.WriteToFileAsync(directoryPath, _lastTree, stoppingToken);
+                
+                if (stoppingToken.IsCancellationRequested) break;
+                chunksProcessed++;
             }
-            await Task.Delay(1000, stoppingToken);
+        }
+        catch (Exception)
+        {
+            _logger.LogCritical("Worker encountered an unhandled exception and will now stop.");
+        }
+        finally
+        {
+            // Write hand comparison tree to file
+            _lifetime.StopApplication();
         }
     }
 }
